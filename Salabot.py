ca8b989a758c0d7b60ae57b1b24b89571791a961
SeeDashMusic <85347510+SHS666638@users.noreply.github.com>
2021-11-17 14:40:20 +0700

    Salabot Controller Using Python
    Salabot Using 4 Package to server hosting to controller you bot

---
+++ Salabot.py
@@ -0,0 +1,8 @@
+import SalabotDeploy from salabot.deploy
+import SalabotHTML from salabot.html
+import SalabotRegister from salabot.register
+import SalabotMSG from salabot.msg
+salabot.html.set = ["p","br","title"]
+salabot.deploy.TextToken = []
+salabot.msg.welcomesetup("Welcome <br>{USER}</br>!")
+salabot.register.acc_uuid()
